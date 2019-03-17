11a3dc153d9ed471cdcc88c725a7353bbe0d21e2
niktrix <nicky.gurbani@gmail.com>
2019-03-17 21:33:37 +0530

    add logger, and ip endpoint

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/google/logger"
 
 	"github.com/go-redis/redis"
 
@@ -25,21 +26,33 @@ type Configuration struct {
 }
 
 func main() {
+
+	lf, err := os.OpenFile("log.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	if err != nil {
+		logger.Fatalf("Failed to open log file: %v", err)
+	}
+	defer lf.Close()
+	defer logger.Init("LoggerExample", true, true, lf).Close()
+
 	file, err := os.Open("config.json")
 	defer file.Close()
+	if err != nil {
+		logger.Fatal("Error While opening config file", err)
+	}
 	decoder := json.NewDecoder(file)
 	configuration := Configuration{}
 	err = decoder.Decode(&configuration)
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err)
 	}
-	fmt.Println(configuration)
 	redisClient = redis.NewClient(&redis.Options{
 		Addr: configuration.Redis.Ip + ":" + strconv.Itoa(configuration.Redis.Port),
 		DB:   0,
 	})
 	r := mux.NewRouter()
-	r.HandleFunc("/dns", dns)
+	r.HandleFunc("/dns", getDNS)
+	r.HandleFunc("/ip", getIP)
+
 	http.Handle("/", r)
 
 	srv := &http.Server{
@@ -48,15 +61,14 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	fmt.Println("Server running at", configuration.Server)
+	logger.Infoln("Server running at", configuration.Server)
 
-	log.Fatal(srv.ListenAndServe())
+	logger.Fatal(srv.ListenAndServe())
 }
 
-func dns(w http.ResponseWriter, r *http.Request) {
+func getDNS(w http.ResponseWriter, r *http.Request) {
 	var dnsServerips []string
 	vars := r.URL.Query().Get("r")
-	fmt.Println(vars)
 	dnsip := redisClient.Keys(vars + ":*")
 	for _, v := range dnsip.Val() {
 		val := redisClient.Get(v)
@@ -64,5 +76,11 @@ func dns(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	js, _ := json.Marshal(dnsServerips)
+	w.Write(js)
+}
+
+func getIP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	js, _ := json.Marshal(strings.Split(r.RemoteAddr, ":")[0])
 	w.Write(js)
 }
